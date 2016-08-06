556b2cd59ab156c106b1736abee5eac88f3653e8
ghvik <vreddy@ischool.berkeley.edu>
2016-08-06 14:18:47 -0700

    Finished ch7 ex02, evaluating python code in a script

--- HW04_ch07_ex02.py
+++ HW04_ch07_ex02.py
@@ -21,15 +21,32 @@
 
 ###############################################################################
 # Imports
-
+import pdb
 
 # Body
-
+def eval_loop():
+    result = ""
+    while True:
+        # Prompt user
+        prompt = ("Please type some Python code to evaluate or type 'done' to finish:\n")
+        user_input = input(prompt)
+        
+        # Evaluate result
+        if user_input != "done":
+            try:
+                result = eval(user_input)
+            except (SyntaxError, NameError) as e:
+                print("Please check if there are any syntax errors or undefined variables in your code.", e)
+            print(result)
+        else:
+            return result
 
 ###############################################################################
 def main():
-    pass  # Remove this line and uncomment below once eval_loop is defined.
-    # eval_loop()
+    eval_loop()
+    
+    # Uncomment to test the return value of the last evaluated expression
+    # print(eval_loop()) 
 
 if __name__ == '__main__':
     main()
