d33e0f1d6ecc3e38263918896148449a2fcf1517
ghvik <vreddy@ischool.berkeley.edu>
2016-08-06 13:18:38 -0700

    Finished exercise 00, guessing the number

--- D04_ex00.py
+++ D04_ex00.py
@@ -11,19 +11,38 @@
 #         - then ends the program
 ################################################################################
 # Imports
-
+from random import randint
 
 # Body
-
-
+def guess_the_number():
+    num = randint(1, 25)
+    prompt = ""
+    for n in range(1,6)[::-1]:
+        prompt = ("Please guess a number from 1 to 25, inclusive. You have {} {} left. "
+            .format(n, "tries" if n > 1 else "try"))
+        try:
+            user_input = int(input(prompt))
+        except ValueError:
+            print("Sorry, that's not a number. Please enter it using digits.")
+        else:
+            if num == user_input:
+                print("Congrats, you guessed it right!")
+                break
+            if n == 1:
+                # Do not give the user another hint if they are out of tries
+                continue
+            if num > user_input:
+                print("Please guess a higher number. ")
+            else:
+                print("Please guess a lower number. ")
+    else:
+        print("Sorry, you are out of tries.")
+        
 
 
 ################################################################################
 def main():
-
-
-    print("Hello World!") # Remove this and replace with your function calls
-    
+    guess_the_number()
 
 if __name__ == '__main__':
     main()
