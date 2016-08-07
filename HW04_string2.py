a520eeb1d20493cddc085bcd57ab2dbf5caf0852
ghvik <vreddy@ischool.berkeley.edu>
2016-08-07 12:51:48 -0700

    Finished HW04 string2, difference exercises with python strings

--- HW04_string2.py
+++ HW04_string2.py
@@ -7,7 +7,7 @@
 # http://code.google.com/edu/languages/google-python-class/
 
 # Additional basic string exercises
-
+import pdb
 
 # D. verbing
 # Given a string, if its length is at least 3,
@@ -17,9 +17,13 @@
 # If the string length is less than 3, leave it unchanged.
 # Return the resulting string.
 def verbing(s):
-    # +++your code here+++
-    return
-
+    if len(s) >= 3:
+        if s[-3:] == 'ing':
+            return s + 'ly'
+        else:
+            return s + 'ing'
+    else:
+        return s
 
 # E. not_bad
 # Given a string, find the first appearance of the
@@ -30,8 +34,33 @@ def verbing(s):
 # So 'This dinner is not that bad!' yields:
 # This dinner is good!
 def not_bad(s):
-    # +++your code here+++
-    return
+    # Check if 'not' appears.
+    not_index = 0
+    not_last_index = 3
+    not_appears = False
+    while not_last_index <= len(s):
+        if s[not_index : not_last_index] == "not":
+            not_appears = True
+            break
+        not_index += 1
+        not_last_index += 1
+    
+    # Check if 'bad' appears.
+    bad_index = 0
+    bad_last_index = 3
+    bad_appears = False
+    while bad_last_index <= len(s):
+        if s[bad_index : bad_last_index] == "bad":
+            bad_appears = True
+            break
+        bad_index += 1
+        bad_last_index += 1
+
+    # Check if both words appear in the correct order.
+    if not_appears and bad_appears and bad_index > not_last_index:
+        return s[:not_index] + "good" + s[bad_last_index:]
+    else:
+        return s
 
 
 # F. front_back
@@ -42,10 +71,31 @@ def not_bad(s):
 # Given 2 strings, a and b, return a string of the form
 #  a-front + b-front + a-back + b-back
 def front_back(a, b):
-    # +++your code here+++
-    return
-
-
+    # Initialize variables.
+    a_front = ""
+    a_back = ""
+    b_front = ""
+    b_back = ""
+    
+    # Divide a into 2 halves.
+    if len(a) % 2 == 0:
+        a_front = a[:len(a) // 2]
+        a_back = a[len(a) // 2 :]
+    else:
+        a_front = a[:len(a) // 2 + 1]
+        a_back = a[len(a) // 2 + 1:]
+        
+    # Divide b into 2 halves.
+    if len(b) % 2 == 0:
+        b_front = b[:len(b) // 2]
+        b_back = b[len(b) // 2 :]
+    else:
+        b_front = b[:len(b) // 2 + 1]
+        b_back = b[len(b) // 2 + 1:]
+        
+    # Combine each half into the final string
+    return a_front + b_front + a_back + b_back
+        
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
 def test(got, expected):
@@ -66,6 +116,7 @@ def main():
 
     print()
     print('not_bad')
+    test(not_bad("bad"), "bad")
     test(not_bad('This movie is not so bad'), 'This movie is good')
     test(not_bad('This dinner is not that bad!'), 'This dinner is good!')
     test(not_bad('This tea is not hot'), 'This tea is not hot')
